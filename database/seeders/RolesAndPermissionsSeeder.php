2d9ac4b429d4061a96f11fbf99facc7d9e3f4db8
Oleksandr Perebykovskyi <mobile322@gmail.com>
2024-11-14 16:56:27 +0100

    users-roles

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $permissions = [
+            'view users',
+            'create users',
+            'edit users',
+            'delete users',
+            'view roles',
+            'create roles',
+            'edit roles',
+            'delete roles',
+            'view movies',
+            'create movies',
+            'edit movies',
+            'delete movies',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission, 'guard_name' => 'web']);
+        }
+
+        $adminRole = Role::firstOrCreate(['name' => 'admin', 'guard_name' => 'web']);
+        $managerRole = Role::firstOrCreate(['name' => 'manager', 'guard_name' => 'web']);
+
+        $adminRole->syncPermissions(Permission::all());
+
+
+        $managerPermissions = [
+            'view users',
+            'view movies',
+            'create movies',
+            'edit movies',
+        ];
+
+        foreach ($managerPermissions as $permission) {
+            $managerRole->givePermissionTo($permission);
+        }
+    }
+}
